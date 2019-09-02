616cc42fbec6f323fc4eda5f3f6bc81d93590064
tkw <894766704@qq.com>
2019-09-02 13:59:08 +0800

    vue-blog;react-admin管理;node后台

---
+++ blog-vue-typescript/src/components/footer.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div class="footer">  ©2018 Created by TKW </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class Footer extends Vue {}
+</script>
+<style scoped>
+.footer {
+  text-align: center;
+  padding: 20px;
+  font-weight: bold;
+}
+</style>
+
